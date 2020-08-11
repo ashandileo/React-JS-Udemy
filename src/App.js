36de9df54e5c4e478569ceeb8176a67a7d274e41
ashandileo <ashandileonadi2001@gmail.com>
2020-08-11 22:45:05 +0700

    deleted Errorboundary file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -53,13 +52,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map( ( person, index ) => {
-            return <ErrorBoundary key={person.id}>
+            return (
               <Person
                 click={() => this.deletePersonHandler( index )}
                 name={person.name}
                 age={person.age}
-                changed={( event ) => this.nameChangedHandler( event, person.id )} />
-            </ ErrorBoundary>
+                changed={( event ) => this.nameChangedHandler( event, person.id )}
+              />
+            )
           } )}
         </div>
       );
