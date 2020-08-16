021ca9510ae3ceed42c7731f2d4cea7520491179
ashandileo <ashandileonadi2001@gmail.com>
2020-08-16 16:56:27 +0700

    calculated ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,22 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
-console.log("props", props.ingredients)
-  const transformedIngredients = Object.keys(props.ingredients)
+
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + 1} type={igKey} />
       })
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+    console.log("transformedIngredients", transformedIngredients)
 
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+    
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
