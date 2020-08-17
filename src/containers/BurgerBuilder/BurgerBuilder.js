78cb52fa5b2014ce7a0444d3d14b91d87f1c24eb
ashandileo <ashandileonadi2001@gmail.com>
2020-08-18 00:41:10 +0700

    send post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICE = {
   salad: 0.5,
@@ -77,7 +78,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('Ypu continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Ashandi Leonadi',
+        address: {
+          street: 'disini',
+          zipCode: '3123',
+          country: 'Indonesia'
+        }
+      },
+      email: 'test@test.com',
+      deliveryMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
   
   render() {
