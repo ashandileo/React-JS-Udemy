28a09d373a532eea9c3c8d9e6f2fb54f375a11f6
ashandileo <ashandileonadi2001@gmail.com>
2020-09-12 20:56:46 +0700

    using history push like link

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -11,7 +11,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
+    // this.props.history.push('/' + id);
+    this.props.history.push({pathname: '/' + id});
   }
 
   componentDidMount() {
@@ -37,13 +38,14 @@ class Posts extends Component {
     if(!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Link to={'/' + post.id} key={post.id}>
+          // <Link to={'/' + post.id} key={post.id}>
             <Post 
+              key={post.id}
               title={post.title}
               author={post.author}
               clicked={() => this.postSelectedHandler(post.id)}
             />
-          </Link>
+          // </Link>
         )
       })
     }
