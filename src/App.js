6d9796daf07ff444f4737b74d04b938ce9d08066
ashandileo <ashandileonadi2001@gmail.com>
2020-09-13 11:05:28 +0700

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Layout>
+            <Switch>
+              <Route path="/" component={BurgerBuilder} exact />
+              <Route path="/checkout" component={Checkout} />
+            </Switch>
+          </Layout>
+        </div>
+      </BrowserRouter>
     );
   }
 }
