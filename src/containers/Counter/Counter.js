30ffd8fbe8fdd2a79398c7f7146f6780636f5278
ashandileo <ashandileonadi2001@gmail.com>
2020-10-17 17:09:35 +0700

    combine multiple reducer

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -7,6 +7,7 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
     render () {
+        console.log("Props", this.props)
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
@@ -15,7 +16,7 @@ class Counter extends Component {
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 10" clicked={this.props.onSubractCounter}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
                         <li
@@ -33,18 +34,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
+    console.log("dispatch", dispatch)
     return {
         onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
         onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
         onAddCounter: () => dispatch({type: actionTypes.ADD, value: 10}),
         onSubractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 10}),
-        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result}),
         onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultId: id}),
     }
 }
