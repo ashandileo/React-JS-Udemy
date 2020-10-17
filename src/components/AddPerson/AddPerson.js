7acc0e53806774ee0e130d8362b48ae19741c711
ashandileo <ashandileonadi2001@gmail.com>
2020-10-17 23:09:33 +0700

    handle user input and add to store

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+const addPerson = (props) => {
+    const [state, setState] = useState({
+        name: '',
+        age: null
+    })
+
+    const nameChangeHandler = (event) => {
+        setState({
+            ...state,
+            name: event.target.value
+        })
+    }
+
+    const ageChangeHandler = (event) => {
+        setState({
+            ...state,
+            age: event.target.value
+        })
+    }
+
+    return (
+        <div className="AddPerson">
+            <input 
+                type="text"
+                placeholder="Name"
+                onChange={nameChangeHandler}
+                value={state.name}
+            />
+            <input
+                type="number"
+                placeholder="Age"
+                onChange={ageChangeHandler}
+                value={state.age}
+            />
+            <button onClick={() => props.personAdded(state.name, state.age)}>Add Person</button>
+        </div>
+    );
+}
 
 export default addPerson;
