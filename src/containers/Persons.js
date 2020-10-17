7f7d8dafb6f059e64bcb3feb4dc9d7ab8f7ae837
ashandileo <ashandileonadi2001@gmail.com>
2020-10-17 20:16:26 +0700

    added logic to update state on redux

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -2,43 +2,39 @@ import React, { Component } from 'react';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+import { connect } from 'react-redux'
+
+import * as actionTypes from './../reducer/actions';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
     render () {
+        console.log(this.props)
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.persons.map(person => (
                     <Person 
                         key={person.id}
                         name={person.name} 
                         age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.onRemovedPerson(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+    return {
+        persons: state.persons
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddedPerson: () => dispatch({ type: actionTypes.ADD_PERSON }),
+        onRemovedPerson: personId => dispatch({ type: actionTypes.REMOVE_PERSON, personId })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -7,12 +7,21 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_PERSON:
+      const newPerson = [
+        {
+            id: Math.random(), // not really unique but good enough here!
+            name: 'Max',
+            age: Math.floor( Math.random() * 40 )
+        }
+      ]
       return {
-
+        ...state,
+        persons: [...state.persons, ...newPerson]
       }
     case actionTypes.REMOVE_PERSON:
       return {
-        
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.personId)
       }
   }
   return state;
