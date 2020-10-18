77e1cf923d3250ed81d64badb64d0dfdee5ccf54
ashandileo <ashandileonadi2001@gmail.com>
2020-10-18 12:52:45 +0700

    enabled order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -15,7 +15,6 @@ import * as actionTypes from '../../store/actions';
 class BurgerBuilder extends Component {
 
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -29,7 +28,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el
       }, 0);
-    this.setState({purchasable: sum > 0});
+    return sum > 0
   }
 
   purchaseHandler = () => {
@@ -87,7 +86,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             price={this.props.totalPrice}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ingredients)}
             ordered={this.purchaseHandler}
           />
         </Aux>
