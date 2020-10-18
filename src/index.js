4748da53f13e6287b5ee55226999580435c7a5ff
ashandileo <ashandileonadi2001@gmail.com>
2020-10-18 23:14:05 +0700

    restructuring folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer  from './store/reducer';
+import reducer  from './store/reducer/burgerBuilder';
 
 const reduxDevToolsExt = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(reducer, reduxDevToolsExt);
