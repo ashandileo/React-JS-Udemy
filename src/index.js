463f01f06dbcbde593428097290ae57733b159a3
ashandileo <ashandileonadi2001@gmail.com>
2020-10-18 20:40:23 +0700

    renamed files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import counterReducer from './store/reducers/counter';
-import resultsReducer from './store/reducers/results';
+import resultsReducer from './store/reducers/result';
 
 import thunk from 'redux-thunk';
 
