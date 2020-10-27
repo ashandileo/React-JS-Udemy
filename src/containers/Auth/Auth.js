5c9aeea1feaeb95d8a5ad8ea2471edb9f0a8bb44
ashandileo <ashandileonadi2001@gmail.com>
2020-10-27 22:36:46 +0700

    Use auth action on Auth Component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
 
 class Auth extends Component {
   state = {
@@ -67,6 +69,13 @@ class Auth extends Component {
     this.setState({controls: updatedControls})
   }
 
+  submitHandler = e => {
+    e.preventDefault()
+    const email = this.state.controls.email.value;
+    const password = this.state.controls.password.value;
+    this.props.onAuth(email, password)
+  }
+
   render () {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -78,7 +87,7 @@ class Auth extends Component {
 
     return (
       <div>
-        <form className={classes.Auth}>
+        <form className={classes.Auth} onSubmit={this.submitHandler}>
           {formElementsArray.map(formElement => (
             <Input
               key={formElement.id}
@@ -100,4 +109,10 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
